44ea2cb35216f639cf32596f7b61f85cce38219f
farzad kh <farzadkh900@gmail.com>
2025-04-09 14:15:15 +0330

    fix(index):add express and  socket.io dependencies

--- index.js
+++ index.js
@@ -1,58 +1,38 @@
-const { createServer } = require("http");
-const next = require("next");
+const express = require("express");
+const http = require("http");
 const { Server } = require("socket.io");
 
+const app = express();
+const server = http.createServer(app);
 
+const io = new Server(server, {
+  cors: {
+    origin: "https://homevisions.vercel.app",
+  },
+});
 
+const users = new Map();
 
-  const port = parseInt(process.env.PORT || "3000", 10);
-  const dev = process.env.NODE_ENV !== "production";
-  const app = next({ dev });
- 
+io.on("connection", (socket) => {
+  console.log("Socket connected:", socket.id);
 
- 
-const hostname = "0.0.0.0";
- 
-
- 
-const handler =  app.getRequestHandler();
-
-const users = new Map(); // userId -> socketId
-
-app.prepare().then(() => {
-  const httpServer = createServer(handler);
-
-  const io = new Server(httpServer, {
-    cors: {
-      origin: "*",  
-    },
+  socket.on("join", (userId) => {
+    users.set(userId, socket.id);
+    console.log(`User ${userId} joined with socket ID ${socket.id}`);
   });
 
-
-  global.io = io;
-  global.users = users;
-
-  io.on("connection", (socket) => {
-    console.log("Socket connected:", socket.id);
-
-    socket.on("join", (userId) => {
-      users.set(userId, socket.id);
-      console.log(`User ${userId} joined with socket ID ${socket.id}`);
-    });
-
-    socket.on("disconnect", () => {
-      for (const [userId, socketId] of users.entries()) {
-        if (socketId === socket.id) {
-          users.delete(userId);
-          console.log(`User ${userId} disconnected`);
-          break;
-        }
+  socket.on("disconnect", () => {
+    for (const [userId, socketId] of users.entries()) {
+      if (socketId === socket.id) {
+        users.delete(userId);
+        console.log(`User ${userId} disconnected`);
+        break;
       }
-    });
-  });
-
-  httpServer.listen(port, () => {
-    console.log(`> Ready on http://${hostname}:${port}`);
+    }
   });
 });
- 
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Socket server running on port ${PORT}`);
+});
